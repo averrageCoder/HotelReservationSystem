e7d4938df5e5ef304f2f449cfc53c8693e062eb7
Harsh R Jain <rjainharsh@gmail.com>
2021-09-16 08:39:42 +0530

    ADDED: to manage the hotel reservation system

--- lib/src/main/java/com/bridgelabz/hotelreservationsystem/MainSystem.java
+++ lib/src/main/java/com/bridgelabz/hotelreservationsystem/MainSystem.java
@@ -1,11 +1,29 @@
 package com.bridgelabz.hotelreservationsystem;
 
+import java.util.ArrayList;
+
 public class MainSystem {
+	
+	private static ArrayList<Hotel> hotelList = new ArrayList<>();
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to hotel reservation system!");
-
+		
+		
+	}
+	
+	public static void addHotel(String hotelName, double rateForRegularCustomer) {
+		
+		Hotel hotel = new Hotel();
+		hotel.setHotelName(hotelName);
+		hotel.setRateForRegularCustomer(rateForRegularCustomer);
+		hotelList.add(hotel);
+		
+	}
+	
+	public static ArrayList<Hotel> getHotelList() {
+		return hotelList;
 	}
 
 }
