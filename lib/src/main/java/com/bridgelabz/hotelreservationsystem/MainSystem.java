94b18b5c49258638a718ef2382f8004cf1d54df3
Harsh R Jain <rjainharsh@gmail.com>
2021-09-16 09:25:58 +0530

    ADDED: method to find cheapest hotel

--- lib/src/main/java/com/bridgelabz/hotelreservationsystem/MainSystem.java
+++ lib/src/main/java/com/bridgelabz/hotelreservationsystem/MainSystem.java
@@ -1,6 +1,12 @@
 package com.bridgelabz.hotelreservationsystem;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 public class MainSystem {
 	
@@ -13,13 +19,22 @@ public class MainSystem {
 		
 	}
 	
-	public void addHotel(String hotelName, double rateForRegularCustomer) {
+	public Hotel getCheapestHotel(LocalDateTime startDate, LocalDateTime endDate) {
 		
+		long noOfDaysBetween = ChronoUnit.DAYS.between(startDate, endDate);
+		
+		ArrayList<Hotel> hotel = hotelList.stream().sorted(Comparator.comparing(Hotel::getRateForRegularCustomer))
+		.collect(Collectors.toCollection(ArrayList::new));
+		
+		return hotel.get(0);
+		
+	}
+	
+	public void addHotel(String hotelName, double rateForRegularCustomer) {
 		Hotel hotel = new Hotel();
 		hotel.setHotelName(hotelName);
 		hotel.setRateForRegularCustomer(rateForRegularCustomer);
-		this.hotelList.add(hotel);
-		
+		this.hotelList.add(hotel);	
 	}
 	
 	public ArrayList<Hotel> getHotelList() {
