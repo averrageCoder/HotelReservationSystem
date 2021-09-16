b707f2e9c653196575654f9a65d272ce6db62f25
Harsh R Jain <rjainharsh@gmail.com>
2021-09-17 02:00:38 +0530

    ADDED: class for custom exception types

---
+++ lib/src/main/java/com/bridgelabz/hotelreservationsystem/HotelException.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.hotelreservationsystem;
+
+public class HotelException extends RuntimeException {
+
+	enum ExceptionType {
+		ENTERED_NULL, ENTERED_EMPTY, INVALID_DATE
+	}
+	ExceptionType type;
+	
+	public HotelException(ExceptionType type, String message) {
+		super(message);
+		this.type = type;
+	}
+	
+	
+}
