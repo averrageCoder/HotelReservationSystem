aa0054ecf461de2e73a87e5879e5cfba67e8c0a6
Harsh R Jain <rjainharsh@gmail.com>
2021-09-16 18:52:50 +0530

    ADDED: files for reward customers

--- lib/src/main/java/com/bridgelabz/hotelreservationsystem/Hotel.java
+++ lib/src/main/java/com/bridgelabz/hotelreservationsystem/Hotel.java
@@ -4,8 +4,10 @@ import java.time.LocalDate;
 public class Hotel {
 
 	private String hotelName;
-	private double rateForWeekDay;
-	private double rateForWeekEnd;
+	private double rateForWeekDayForRegularCustomer;
+	private double rateForWeekEndForRegularCustomer;
+	private double rateForWeekDayForRewardCustomer;
+	private double rateForWeekEndForRewardCustomer;
 	private int ratings;
 	
 	public int getRatings() {
@@ -20,34 +22,41 @@ public class Hotel {
 	public void setHotelName(String hotelName) {
 		this.hotelName = hotelName;
 	}
-	public double getRateForWeekDay() {
-		return rateForWeekDay;
+	public double getRateForWeekDayForRegularCustomer() {
+		return rateForWeekDayForRegularCustomer;
 	}
-	public void setRateForWeekDay(double rateForWeekDay) {
-		this.rateForWeekDay = rateForWeekDay;
+	public void setRateForWeekDayForRegularCustomer(double rateForWeekDay) {
+		this.rateForWeekDayForRegularCustomer = rateForWeekDay;
 	}
-	public double getRateForWeekEnd() {
-		return rateForWeekEnd;
+	public double getRateForWeekEndForRegularCustomer() {
+		return rateForWeekEndForRegularCustomer;
 	}
-	public void setRateForWeekEnd(double rateForWeekEnd) {
-		this.rateForWeekEnd = rateForWeekEnd;
+	public void setRateForWeekEndForRegularCustomer(double rateForWeekEnd) {
+		this.rateForWeekEndForRegularCustomer = rateForWeekEnd;
 	}
-	public double getRateForDates(LocalDate startDate, LocalDate endDate) {
+	public double getRateForWeekDayForRewardCustomer() {
+		return rateForWeekDayForRewardCustomer;
+	}
+	public void setRateForWeekDayForRewardCustomer(double rateForWeekDayForRewardCustomer) {
+		this.rateForWeekDayForRewardCustomer = rateForWeekDayForRewardCustomer;
+	}
+	public double getRateForWeekEndForRewardCustomer() {
+		return rateForWeekEndForRewardCustomer;
+	}
+	public void setRateForWeekEndForRewardCustomer(double rateForWeekEndForRewardCustomer) {
+		this.rateForWeekEndForRewardCustomer = rateForWeekEndForRewardCustomer;
+	}
+	public double getRegularRateForDates(LocalDate startDate, LocalDate endDate) {
 		double totalRate = 0;
 		
 		for (LocalDate date = startDate; date.isBefore(endDate.plusDays(1)); date = date.plusDays(1)) {
 		    if(date.getDayOfWeek().getValue() <= 5) {
-		    	totalRate+=this.rateForWeekDay;
+		    	totalRate+=this.rateForWeekDayForRegularCustomer;
 		    }
 		    else {
-		    	totalRate+=this.rateForWeekEnd;
+		    	totalRate+=this.rateForWeekEndForRegularCustomer;
 		    }
 		}
 		return totalRate;
-	}
-	@Override
-	public String toString() {
-		return "Hotel [hotelName=" + hotelName + ", rateForWeekDay=" + rateForWeekDay + ", rateForWeekEnd="
-				+ rateForWeekEnd + ", ratings=" + ratings + "]";
 	}
 }
