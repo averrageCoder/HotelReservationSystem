1dbb1c0f31a36b616a171ecf202a350904d440f9
Harsh R Jain <rjainharsh@gmail.com>
2021-09-16 08:45:21 +0530

    ADDED: test for hotel list size

--- lib/src/test/java/com/bridgelabz/hotelreservationsystem/HotelTest.java
+++ lib/src/test/java/com/bridgelabz/hotelreservationsystem/HotelTest.java
@@ -24,4 +24,12 @@ public class HotelTest {
 		assertEquals(100, hotelList.get(0).getRateForRegularCustomer());
 	}
 	
+	@Test
+	public void givenHotelList_shouldReturnProperSize() throws Exception {
+		MainSystem hotelReservationSystem = new MainSystem();
+		hotelReservationSystem.addHotel("bridge", 100);
+		ArrayList<Hotel> hotelList = hotelReservationSystem.getHotelList();
+		assertEquals(1, hotelList.size());
+	}
+	
 }
