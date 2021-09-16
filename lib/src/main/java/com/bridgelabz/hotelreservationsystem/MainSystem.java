3e4c29e95cfc486172722c946bf1afb898db1b37
Harsh R Jain <rjainharsh@gmail.com>
2021-09-16 08:17:06 +0530

    ADDED: initialization of project structure

---
+++ lib/src/main/java/com/bridgelabz/hotelreservationsystem/MainSystem.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.hotelreservationsystem;
+
+public class MainSystem {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to hotel reservation system!");
+
+	}
+
+}
